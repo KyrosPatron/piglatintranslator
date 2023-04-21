857e6d08d83010c616b77c0a91ee06055a0c67b0
KyrosTycheros <cirofortunato95@gmail.com>
2023-04-21 09:47:46 +0200

    User Storie #1 already implemented , US #2 just implemented

--- test/piglatintranslator/TranslatorTest.java
+++ test/piglatintranslator/TranslatorTest.java
@@ -11,5 +11,12 @@ public class TranslatorTest {
 		Translator translator = new Translator ("Hello World");
 		assertEquals("Hello World", translator.getPhrase());
 	}
+	
+	@Test
+	public void testTranslationEmptyPhrase() throws Exception {
+		Translator translator = new Translator ("");
+		assertEquals("nil", translator.translate());
+	}
+	
 
 }
